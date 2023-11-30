63b7b7f60e2024984cb7d509fbba834467a2c976
suttonet <149020496+suttonet@users.noreply.github.com>
2023-11-30 13:38:37 -0500

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,15 +1,15 @@
 #include <gtest/gtest.h>
-#include "PokemonFactory.h"
+//#include "PokemonFactory.h"
 
 TEST(PokemonBattle, Battle1)
 {
-    std::shared_ptr<Pokemon> myPokemon = PokemonFactory::createPokemon(100, 120, 80, "HighAttack", "Water");
+    //std::shared_ptr<Pokemon> myPokemon = PokemonFactory::createPokemon(100, 120, 80, "HighAttack", "Water");
 
-    std::cout << "Role: " << myPokemon->GetRole() << std::endl;
-    std::cout << "Type: " << myPokemon->GetType() << std::endl;
-    std::cout << "HP: " << myPokemon->GetHP() << std::endl;
-    std::cout << "Attack: " << myPokemon->GetAttack() << std::endl;
-    std::cout << "Defense: " << myPokemon->GetDefense() << std::endl;
+    //std::cout << "Role: " << myPokemon->GetRole() << std::endl;
+   // std::cout << "Type: " << myPokemon->GetType() << std::endl;
+   // std::cout << "HP: " << myPokemon->GetHP() << std::endl;
+   // std::cout << "Attack: " << myPokemon->GetAttack() << std::endl;
+   // std::cout << "Defense: " << myPokemon->GetDefense() << std::endl;
 }
 
 
